f05790e22fd361ed3139335f1fc447d8812976f1
ELIZABETH BEALE <EGBEALE@GMAIL.COM>
2022-06-14 20:38:36 -0700

    finished lab2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
-import Main from './Main.js';
+import Main from './Main.js'
 import './App.css';
 
 class App extends React.Component {
@@ -12,7 +12,7 @@ class App extends React.Component {
         <Main/>
         <Footer/>
       </>
-    );
+    )
   }
 }
 
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import './HornedBeast.css'
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+    }
+  }
+
+  likePhoto = () => {
+    this.setState({
+      likes: this.state.likes + 1,
+    })
+  }
+
   render() {
     return(
-      <>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
-        <p>{this.props.description}</p>
-      </>
+      <Card 
+        // border='light' 
+        className='cards'
+      >
+        <Card.Body>
+          <Card.Title className='titles'>{this.props.title}</Card.Title>
+          <Card.Subtitle className='likes'>♡  {this.state.likes}</Card.Subtitle>
+          <Card.Img 
+            src={this.props.imageUrl} 
+            alt={this.props.description} 
+            title={this.props.title} 
+            onClick={this.likePhoto}
+            className='card-image'/>
+          <Card.Text>{this.props.description}</Card.Text>
+        </Card.Body>
+      </Card>
     )
   }
 }
