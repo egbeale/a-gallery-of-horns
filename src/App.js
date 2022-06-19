7ff0e4fab661fcf6ae35af82873ac39e380ec805
ELIZABETH BEALE <EGBEALE@GMAIL.COM>
2022-06-18 19:53:40 -0700

    implemented form component, attempted to filter data but need assistance

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import Main from './Main.js'
+import SearchHorns from './SearchHorns.js'
 import data from './data.json';
 import './App.css';
 import SelectedBeast from './SelectedBeast.js';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       modalActive: false,
-      selectedBeast: ''
-      // set up initial state of selectedBeast object. this.setState({ selectedBeast: selectedBeast })
+      selectedBeast: '',
+      // hornData: data,
     };
   };
 
@@ -28,28 +30,58 @@ class App extends React.Component {
       modalActive: true,
       selectedBeast: beast,
       //passing in real value as an object with properties. methods setState setting parameters.
-      // ?? why did the demo have this:
-      // title: title,
-      // image_url: imageUrl
     });
   };
 
 
+  // ------------ FORM INPUT --------------
+
+  // Do I need another event handler here?
+
+  handleSelect = event => {
+    let selected = event.target.value;
+    console.log(selected);
+
+    if (selected === '1') {
+      let filteredData = data.filter(beast => beast.horns === 1);
+      this.setState({
+        hornData: filteredData});
+      console.log(this);
+    } else if (selected=== '2') {
+      let filteredData = data.filter(beast => beast.horns === 2);
+      this.setState({
+        hornData: filteredData
+      });
+    } else if (selected === '3') {
+      let filteredData = data.filter(beast => beast.horns === 3);
+      this.setState({
+        hornData: filteredData
+      });
+    } else if (selected === '100') {
+      let filteredData = data.filter(beast => beast.horns > 3);
+      this.setState({
+        hornData: filteredData
+      });
+    } else {
+      this.setState({
+        hornData: data
+      });
+    }
+  }
 
   render() {
     return (
       <>
         <Header/>
+        <SearchHorns handleSelect={this.handleSelect}/> 
         <Main 
           data={data}
           handleOpenModal={this.handleOpenModal} // Going thru Main.js to send it to HB.js 
+          hornData={this.state.hornData}
         />
         <SelectedBeast // sending this into SelectedBeast 
           modalActive={this.state.modalActive}
           modalClose={this.handleCloseModal}
-          // title={this.state.title}
-          // image_url={this.state.imageUrl}
-          // description={this.state.description}
           selectedBeast={this.state.selectedBeast}
         />
         <Footer/>
